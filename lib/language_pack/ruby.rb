8837949fd82fa5d3e8735db7aca479de7be4ce17
Honza Hovorka <honza.hovorka@gmail.com>
2016-10-20 07:16:50 +0200

    Add FreeTDS lib to default_config_vars

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -56,7 +56,8 @@ class LanguagePack::Ruby < LanguagePack::Base
   def default_config_vars
     instrument "ruby.default_config_vars" do
       vars = {
-        "LANG" => env("LANG") || "en_US.UTF-8"
+        "LANG" => env("LANG") || "en_US.UTF-8",
+        "LD_LIBRARY_PATH" => "vendor/freetds/lib:$LD_LIBRARY_PATH"
       }
 
       ruby_version.jruby? ? vars.merge({
