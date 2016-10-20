df0a7b6dbfe0934762f62ce67819960606a9e585
Honza Hovorka <honza.hovorka@gmail.com>
2016-10-20 07:28:54 +0200

    Add override to LD_LIBRARY_PATH with FreeTDS

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -312,6 +312,7 @@ SHELL
       set_env_default  "LANG",     "en_US.UTF-8"
       set_env_override "GEM_PATH", "$HOME/#{slug_vendor_base}:$GEM_PATH"
       set_env_override "PATH",     binstubs_relative_paths.map {|path| "$HOME/#{path}" }.join(":") + ":$PATH"
+      set_env_override "LD_LIBRARY_PATH", "vendor/freetds/lib:$LD_LIBRARY_PATH"
 
       add_to_profiled set_default_web_concurrency if env("SENSIBLE_DEFAULTS")
 
